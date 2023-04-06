11fbcfb715d59bf9285bb04fd1311502eca87095
Anto <japanto20@gmail.com>
2023-04-06 16:15:18 -0500

    create servers script updated

--- frontend.sh
+++ frontend.sh
@@ -10,29 +10,24 @@ status_check $?
 
 print_head "Downloading Frontend Content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
-echo $?
 status_check $?
 
 print_head "Extracting Downloaded Frontend Content"
 # shellcheck disable=SC2164
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
-echo $?
 status_check $?
 
 print_head "Copying Nginx Config for Roboshop"
 
 cp ${code_dir}/configs/nginx-roboshop.conf  /etc/nginx/default.d/roboshop.conf &>>${log_file}
-echo $?
 status_check $?
 
 print_head "Enabling nginx"
 systemctl enable nginx &>>${log_file}
-echo $?
 status_check $?
 
 print_head "Starting nginx"
 systemctl start nginx &>>${log_file}
-echo $?
 # shellcheck disable=SC2181
 status_check $?
