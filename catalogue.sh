5087b26c78fb82023d028a1a50d691a61a3d1993
Anto <japanto20@gmail.com>
2023-04-06 12:07:12 -0500

    create servers script updated

--- catalogue.sh
+++ catalogue.sh
@@ -1,38 +1,48 @@
 source common.sh
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+print_head "Configure NodeJS Repo"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
 
 print_head "Installing nodejs"
-yum install nodejs -y
+yum install nodejs -y &>>${log_file}
 
-print_head "Adding User Roboshop"
-useradd roboshop
+print_head "Creating User Roboshop"
+useradd roboshop &>>${log_file}
 
-print_head "Creating Dir"
-mkdir /app
+print_head "Creating Application Directory"
+mkdir /app &>>${log_file}
 
-print_head "Removing old file from Dir"
-rm -rf /app/*
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+print_head "Removing old files from Dir"
+rm -rf /app/* &>>${log_file}
+
+print_head "Downloading app content"
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
 cd /app
-unzip /tmp/catalogue.zip
+
+print_head "Extracting app content"
+unzip /tmp/catalogue.zip &>>${log_file}
 cd /app
 
 print_head "Installing npm catalogue service"
-npm install
-cp configs/catalogue.service etc/systemd/system/catalogue.service
+npm install &>>${log_file}
 
-print_head "Daemon reloading"
-systemctl daemon-reload
+print_head "Copying SystemD Service File"
+cp configs/catalogue.service etc/systemd/system/catalogue.service &>>${log_file}
+
+print_head "SystemD reloading"
+systemctl daemon-reload &>>${log_file}
 
 print_head "Enable Catalogue service"
-systemctl enable catalogue
+systemctl enable catalogue &>>${log_file}
 
 print_head "Start Catalogue Service"
-systemctl start catalogue
+systemctl restart catalogue &>>${log_file}
 
-print_head "Installing mongodb service"
-cp configs/mongodb.repo etc/yum.repos.d/mongo.repo
-yum install mongodb-org-shell -y
+print_head "Copy mongodb Repo File"
+cp configs/mongodb.repo etc/yum.repos.d/mongo.repo &>>${log_file}
 
-mongo --host catalogue.antodevops20.com </app/schema/catalogue.js
+print_head "Install Mongo Client"
+yum install mongodb-org-shell -y &>>${log_file}
+
+print_head "Load Schema"
+mongo --host catalogue.antodevops20.com </app/schema/catalogue.js &>>${log_file}
