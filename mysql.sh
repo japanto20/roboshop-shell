761e5528f0c8ff1a4318b8f3f064d0a32cc8f705
Anto <japanto20@gmail.com>
2023-04-10 15:42:10 -0500

    create servers script updated

--- mysql.sh
+++ mysql.sh
@@ -28,8 +28,9 @@ systemctl start mysqld &>>${log_file}
 status_check $?
 
 print_head "Set Root Password"
-mysql_secure_installation --set-root-pass ${mysql_root_password} &>>${log_file}
+echi show databases | mysql -uroot -pR${mysql_root_password}   &>>${log_file}
+if [ $? -ne 0 ]; then
+  mysql_secure_installation --set-root-pass ${mysql_root_password} &>>${log_file}
+fi
 status_check $?
 
-mysql -uroot -pRoboShop@1 &>>${log_file}
-status_check $?
